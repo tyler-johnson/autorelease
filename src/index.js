95878379eb448afc0d90ae563bfdc0e908def570
Tyler Johnson <tyler@tylerjohnson.me>
2016-03-02 23:33:36 -0700

    feat: save-version now prep-publish, add .npmrc

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ export {pre,post};
 import fetchLatest from "./steps/fetch-latest";
 import verify from "./steps/verify";
 import nextVersion from "./steps/next-version";
-import saveVersion from "./steps/save-version";
+import prepPublish from "./steps/prep-publish";
 import generateChangelog from "./steps/generate-changelog";
 import publishChangelog from "./steps/publish-changelog";
-export {fetchLatest,verify,nextVersion,saveVersion,generateChangelog,publishChangelog};
+export {fetchLatest,verify,nextVersion,prepPublish,generateChangelog,publishChangelog};
 
 import options from "./options";
 import pipeline from "./pipeline";
