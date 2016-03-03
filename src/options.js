d8bf34f0d9438278f23ea1b31f22c3bca09700a7
Tyler Johnson <tyler@tylerjohnson.me>
2016-03-02 21:32:15 -0700

    feat: add .autoreleaserc file for options

--- src/options.js
+++ src/options.js
@@ -1,14 +1,24 @@
 import fs from "fs-promise";
-import path from "path";
+import {resolve} from "path";
 
 export default async function(opts) {
 	let {basedir=process.cwd()} = opts;
-	let pkgfile = path.resolve(basedir, "package.json");
+	let pkgfile = resolve(basedir, "package.json");
 	let pkg = JSON.parse(await fs.readFile(pkgfile, "utf-8"));
+
+	let rc;
+	try {
+		rc = await fs.readFile(resolve(basedir, ".autoreleaserc"), "utf-8");
+	} catch(e) {
+		if (e.code !== "ENOENT") throw e;
+	}
+
+	if (rc) rc = JSON.parse(rc);
 
 	return {
 		...opts,
 		...pkg.autorelease,
+		...rc,
 		basedir,
 		package: pkg,
 		packageFile: pkgfile
