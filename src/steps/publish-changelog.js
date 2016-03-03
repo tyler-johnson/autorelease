47bc2c99111153df7859a1c15417d6760d1f35ca
Tyler Johnson <tyler@tylerjohnson.me>
2016-03-02 22:10:25 -0700

    fix (publish-changelog): don't commit, append to front

--- src/steps/publish-changelog.js
+++ src/steps/publish-changelog.js
@@ -1,15 +1,24 @@
 import {exec as _exec} from "child_process";
 import promisify from "es6-promisify";
-import fs from "fs-promise";
+import {readFile,writeFile} from "fs-promise";
+import {resolve} from "path";
 
 const exec = promisify(_exec, function(err, res) {
 	if (err) this.reject(err);
 	else this.resolve(Array.isArray(res) ? res[0] : res);
 });
 
-export default async function(log, {package:pkg}) {
+export default async function(log, {package:pkg,basedir="."}) {
+	// tag the current commit with new version
+	// done here because github variant tags and releases all-in-one
 	await exec(`git tag -a v${pkg.version} -m ""`);
-	await fs.writeFile("changelog.md", log, { flag: "a" });
-	await exec(`git add changelog.md`);
-	await exec(`git commit -m "chore: updated changelog for v${pkg.version}"`);
+
+	// write the changelog to a file
+	if (typeof log === "string") {
+		let cfile = resolve(basedir, "changelog.md");
+		let existing = "";
+		try { existing = await readFile(cfile, "utf-8"); }
+		catch(e) { if (e.code !== "ENOENT") throw e; }
+		await writeFile(cfile, existing + log);
+	}
 }
