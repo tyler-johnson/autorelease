d34980e51fff4fd0586f816ad3cf8c838f03f502
Tyler Johnson <tyler@tylerjohnson.me>
2016-03-03 18:25:13 -0700

    feat: configure npm as its own step, before fetching latest

--- src/pre.js
+++ src/pre.js
@@ -4,6 +4,7 @@ import fetchLatest from "./steps/fetch-latest";
 import verify from "./steps/verify";
 import nextVersion from "./steps/next-version";
 import prepPublish from "./steps/prep-publish";
+import configureNpm from "./steps/configure-npm";
 
 export default async function pre(opts={}) {
 	opts = await options(opts);
@@ -11,6 +12,7 @@ export default async function pre(opts={}) {
 
 	if (!Array.isArray(steps)) steps = [
 		steps.verify || verify,
+		steps.configureNpm || configureNpm,
 		steps.fetchLatest || fetchLatest,
 		steps.nextVersion || nextVersion,
 		steps.prepPublish || prepPublish
