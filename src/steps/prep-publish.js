b61dcc53034b5040a45a8b48f4accd7b842d9f13
Tyler Johnson <tyler@tylerjohnson.me>
2016-05-07 11:14:27 -0600

    feat: add npm dist tag support

--- src/steps/prep-publish.js
+++ src/steps/prep-publish.js
@@ -1,10 +1,20 @@
 import {writeFile} from "fs-promise";
+import SemVer from "semver";
 
 // write version to the package.json
 export default async function(version, {
+	tag,
 	package: pkg,
 	packageFile: pkgfile
 }) {
-	pkg.version = version;
+	version = new SemVer(version);
+
+	// set the correct dist tag
+	if (tag || version.prerelease.length) {
+		if (pkg.publishConfig == null) pkg.publishConfig = {};
+		pkg.publishConfig.tag = tag || "edge";
+	}
+
+	pkg.version = version.toString();
 	await writeFile(pkgfile, JSON.stringify(pkg, null, 2));
 }
