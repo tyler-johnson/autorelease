17747662b6682c8cbfde56d820890ffba1754ee1
Tyler Johnson <tyler@tylerjohnson.me>
2016-05-07 10:07:29 -0600

    fix: add some docs

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import babel from "rollup-plugin-babel";
+import json from "rollup-plugin-json";
 
 export default {
 	onwarn: ()=>{},
 	plugins: [
+		json(),
 		babel({
 			exclude: 'node_modules/**'
 		})
