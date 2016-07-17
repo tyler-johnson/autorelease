4a63288f79646067354f1f5da76f8d23ca521894
Tyler Johnson <tyler@tylerjohnson.me>
2016-07-17 10:30:33 -0600

    feaet: new prerelease config

--- src/steps/verify.js
+++ src/steps/verify.js
@@ -3,21 +3,39 @@ import promisify from "es6-promisify";
 const gitBranch = promisify(_gitBranch);
 import {isRegExp} from "lodash";
 
-export default async function(r, {branch,basedir="."}) {
-	if (branch) {
-		let current = await gitBranch(basedir);
-		let pass = [].concat(branch).some(b => {
-			if (isRegExp(b)) {
-				return b.test(current);
-			} else if (typeof b === "string") {
-				return current === b;
-			} else if (typeof b === "function") {
-				return b(current);
-			}
-		});
-
-		if (!pass) {
-			throw new Error(`This autorelease was triggered on branch ${current}, which is not a branch autorelease is configured to publish from.`);
+function satisfiesBranch(tests, branch) {
+	return [].concat(tests).some(b => {
+		if (isRegExp(b)) {
+			return b.test(branch);
+		} else if (typeof b === "string") {
+			return branch === b;
+		} else if (typeof b === "function") {
+			return b(branch);
 		}
+	});
+}
+
+export default async function(r, opts={}) {
+	let {branch,prerelease,basedir="."} = opts;
+	let current = await gitBranch(basedir);
+
+	// prerelease as a string is enabled by default
+	if (typeof prerelease === "string") {
+		prerelease = {
+			id: prerelease,
+			enabled: true
+		};
 	}
+
+	// toggle prerelease if prerelease branch matches
+	if (prerelease.branch) {
+		prerelease.enabled = satisfiesBranch(prerelease.branch, current);
+	}
+
+	// throw branch error only if not in prerelease and branch doesn't match
+	if (!prerelease.enabled && branch && !satisfiesBranch(branch, current)) {
+		throw new Error(`This autorelease was triggered on branch ${current}, which is not a branch autorelease is configured to publish from.`);
+	}
+
+	opts.prerelease = prerelease;
 }
