25dc737d8edf22ba4b878331a5973d8d5e2c4c41
Tyler Johnson <tyler@tylerjohnson.me>
2016-10-05 22:17:39 -0600

    fix(plugin-pre): push verify task with no-conflict

--- packages/autorelease-plugin-pre/src/index.js
+++ packages/autorelease-plugin-pre/src/index.js
@@ -6,8 +6,11 @@ import configureNpm from "autorelease-task-configure-npm";
 import prepPublish from "autorelease-task-prep-publish";
 
 export default function(autorelease) {
+  // push verify to end of pipeline (no-conflict mode)
+  autorelease.pipeline("pre.verify").add(verify);
+
+  // set the remaining tasks by name (overwrite mode)
   autorelease.pipeline("pre")
-    .add("verify", verify)
     .add("configureNpm", configureNpm)
     .add("fetchLatest", fetchLatest)
     .add("fetchCommits", fetchCommits)
