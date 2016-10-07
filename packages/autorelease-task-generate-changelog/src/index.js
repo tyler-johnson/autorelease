cf2afa4d2719130274f3ce96b16697b601fa650e
Tyler Johnson <tyler@beneaththeink.com>
2016-10-07 11:59:26 -0600

    fix(task-generate-changelog): generate the changelog the hard way

--- packages/autorelease-task-generate-changelog/src/index.js
+++ packages/autorelease-task-generate-changelog/src/index.js
@@ -1,14 +1,39 @@
-import ccparser from "conventional-changelog";
+import fetchCommits from "autorelease-task-fetch-commits";
+import toStream from "stream-array";
+import ccwriter from "conventional-changelog-writer";
+import mergeConfig from "conventional-changelog-core/lib/merge-config";
+import through from "through2";
 
 export default async function(ctx) {
   const {options={},packageFile} = ctx;
   const {changelog=true,preset} = options;
   if (!changelog) return;
 
-  let clog = ccparser({
+  let config;
+  if (preset) {
+    try {
+      config = require('conventional-changelog-' + preset.toLowerCase());
+    } catch (err) {
+      console.warn('Changelog Preset: "' + preset + '" does not exist');
+    }
+  }
+
+  const conf = await mergeConfig({
     pkg: { path: packageFile },
-    preset
+    config
   });
+
+  const commits = await fetchCommits(ctx, conf.gitRawCommitsOpts, conf.parserOpts);
+
+  const clog = toStream(commits)
+    .pipe(through.obj(function(chunk, enc, cb) {
+      try {
+        conf.options.transform.call(this, chunk, cb);
+      } catch (err) {
+        cb(err);
+      }
+    }))
+    .pipe(ccwriter(conf.context, conf.writerOpts));
 
   let data = "";
   clog.setEncoding("utf-8");
@@ -20,5 +45,6 @@ export default async function(ctx) {
   });
 
   ctx.changelog = data;
-  console.log("Generated changelog");
+  console.log("Generated changelog from %s commits", commits.length);
+  return data;
 }
