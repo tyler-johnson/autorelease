392d13ead01433770b972e3ac9a19f7df62c935b
Tyler Johnson <tyler@beneaththeink.com>
2016-10-10 11:38:35 -0600

    fix(task-prep-publish): alway write package data, don't mutate context

--- packages/autorelease-task-git-tag/src/index.js
+++ packages/autorelease-task-git-tag/src/index.js
@@ -1,7 +1,17 @@
 export default async function(ctx) {
-	const {package:pkg={}} = ctx;
+	const {package:pkg={},version} = ctx;
+	let ver;
+
+	if (version && version.next) {
+		ver = version.next;
+	} else if (pkg.version) {
+		ver = pkg.version;
+	}
+
+	if (!ver) return;
 
 	// tag the current commit with new version
 	// done here because github variant tags and releases all-in-one
-	await ctx.exec(`git tag -a v${pkg.version} -m ""`);
+	await ctx.exec(`git tag -a v${ver} -m ""`);
+	return ver;
 }
--- packages/autorelease-task-prep-publish/src/index.js
+++ packages/autorelease-task-prep-publish/src/index.js
@@ -1,52 +1,30 @@
 import {writeFile} from "fs";
-import {relative} from "path";
 
 // write version to the package.json
 export default async function(ctx) {
-	const {version,package:pkg={},packageFile,tag} = ctx;
-	const wrote = {};
+	const {version,package:pkg,packageFile,tag} = ctx;
+
+	// clone the package data so we don't affect the existing
+	const pkgdata = { ...pkg };
 
 	// set the correct dist tag
 	if (tag) {
-		if (pkg.publishConfig == null) pkg.publishConfig = {};
-		pkg.publishConfig.tag = tag;
-		wrote.tag = true;
+		pkgdata.publishConfig = {
+			...pkgdata.publishConfig,
+			tag: tag
+		};
 	}
 
-	// set new version
-	if (version && version.next) {
-		pkg.version = version.next;
-		wrote.ver = true;
-	}
-
-	// or remove it all together
+	// set new version or remove it all together
 	// this is so a new git tag is not created since there was no new version
-	else {
-		pkg.version = void 0;
-		wrote.ver = true;
-	}
+	pkgdata.version = version && version.next ? version.next : void 0;
 
-	if (wrote.tag || wrote.ver) {
-		// write the package file
-		await new Promise((resolve, reject) => {
-			writeFile(packageFile, JSON.stringify(pkg, null, 2) + "\n", (err) => {
-				err ? reject(err) : resolve();
-			});
+	// write the package file
+	await new Promise((resolve, reject) => {
+		writeFile(packageFile, JSON.stringify(pkgdata, null, 2) + "\n", (err) => {
+			err ? reject(err) : resolve();
 		});
+	});
 
-		let log = "Wrote ";
-		const logargs = [];
-
-		if (wrote.tag) log += "tag";
-		if (wrote.tag && wrote.ver) log += " and ";
-		if (wrote.ver) {
-			log += "version %s";
-			logargs.push(version ? version.next : "empty");
-		}
-
-		log += " to %s";
-		logargs.push(relative((ctx.parentContext && ctx.parentContext.basedir) || ctx.basedir || ".", packageFile));
-
-		console.log(log, ...logargs);
-	}
+	return pkgdata;
 }
