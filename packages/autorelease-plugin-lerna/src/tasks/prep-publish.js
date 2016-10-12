1d44c5483446a8b63f0c20b047caae3dade03008
Tyler Johnson <tyler@beneaththeink.com>
2016-10-12 11:30:34 -0600

    update dep versions in each package

--- packages/autorelease-plugin-lerna/src/tasks/prep-publish.js
+++ packages/autorelease-plugin-lerna/src/tasks/prep-publish.js
@@ -1,11 +1,20 @@
 import createPipeline from "../pipeline";
 import prepPublish from "autorelease-task-prep-publish";
+import {get} from "lodash";
 
 const pipeline = createPipeline();
 export default pipeline;
 
 // write version to outer package.json for post step
 pipeline.add("prepLernaGlobal", async (ctx) => {
+  // get all versions
+  ctx.package_versions = ctx.packages.reduce((v, p) => {
+    let ver = get(p, "autorelease_ctx.version.next");
+    if (!ver) ver = get(p, "autorelease_ctx.latest.version");
+    if (ver) v[p.name] = ver;
+    return v;
+  }, {});
+
   // non-independent mode, bump outer version normally
   if (!ctx.independent) {
     await prepPublish(ctx);
@@ -26,7 +35,17 @@ pipeline.add("prepLernaGlobal", async (ctx) => {
 });
 
 // write new versions to all updated packages
-pipeline.addLernaTask("prepPackages", prepPublish, {
+pipeline.addLernaTask("prepPackages", async (ctx) => {
+  const {package:pkg,package_versions:versions} = ctx;
+  const deps = pkg.dependencies || {};
+
+  pkg.dependencies = Object.keys(deps).reduce((d, key) => {
+    d[key] = versions[key] ? "^" + versions[key] : deps[key];
+    return d;
+  }, {});
+
+  await prepPublish(ctx);
+}, {
   updatedOnly: true,
   forceLoop: true
 });
