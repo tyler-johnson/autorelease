fcda5993db6a8d8343dfcb883db8fdb221f13a31
Tyler Johnson <tyler@tylerjohnson.me>
2017-04-22 09:52:38 -0600

    fix(task-git-remote): add windows support

--- packages/autorelease-task-git-remote/src/index.js
+++ packages/autorelease-task-git-remote/src/index.js
@@ -1,26 +1,29 @@
 import gitUrlParse from "git-url-parse";
-import _gitRemotes from "git-remotes";
-import promisify from "es6-promisify";
-
-const gitRemotes = promisify(_gitRemotes);
 
 export default async function(ctx) {
 	if (ctx.gitUrl) return ctx.gitUrl;
-	const {package:pkg,basedir="."} = ctx;
+	const {package:pkg} = ctx;
 
 	// attempt to find repo url from the package.json
   let repourl = pkg && pkg.repository && pkg.repository.url;
 
 	// otherwise look in git remotes
 	if (!repourl) {
-    const remotes = await gitRemotes(basedir);
+		const rsrc = await ctx.exec("git remote -v");
+		
+		const remotes = rsrc.split(/\r?\n/g)
+			.map(l => l.trim())
+			.filter(l => l.match(/\(fetch\)$/))
+			.map(l => l.split(/\s+/g).slice(0,2))
+			.filter(l => l.length >= 2);
+
 		if (remotes.length && !remotes.some((r) => {
-			if (r.name === "origin") {
-				repourl = r.url;
+			if (r[0] === "origin") {
+				repourl = r[1];
 				return true;
 			}
 		})) {
-			repourl = remotes[0].url;
+			repourl = remotes[0][1];
 		}
 	}
 
